6a8b6b7b6a4c58eae747d1503994ccff28dab96a
Seth Deegan <jayandseth@gmail.com>
2019-05-12 08:15:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,32 +2,90 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const passport = require('passport')
+const session = require('express-session');
 const SteamStrategy = require('passport-steam');
+const authRoutes = require('./routes/auth');
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Steam profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the SteamStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
 passport.use(new SteamStrategy({
     returnURL: 'https://steam-passport.herokuapp.com/auth/steam/return',
     realm: 'https://steam-passport.herokuapp.com/',
     apiKey: process.env.API_KEY
   },
   function(identifier, profile, done) {
-    console.log(profile)
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Steam profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Steam account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
   }
 ));
 
-app.get('/auth/steam',
-  passport.authenticate('steam'),
-  function(req, res) {
-    // The request will be redirected to Steam for authentication, so
-    // this function will not be called.
-  });
+var app = express();
 
-app.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-  
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: 'your secret',
+    name: 'name of session id',
+    resave: true,
+    saveUninitialized: true}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/../../public'));
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+// See views/auth.js for authentication routes
+app.use('/auth', authRoutes);
+
+app.listen(3000);
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
