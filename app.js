7632977a412cf1018ea53156d7189f7332ccfcda
Seth Deegan <jayandseth@gmail.com>
2019-05-11 22:59:50 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const passport = require('passport')
+const SteamStrategy = require('passport-steam');
+
+passport.use(new SteamStrategy({
+    returnURL: 'https://steam-passport.herokuapp.com/auth/steam/return',
+    realm: 'https://steam-passport.herokuapp.com/',
+    apiKey: process.env.API_KEY
+  },
+  function(identifier, profile, done) {
+    User.findByOpenID({ openId: identifier }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.get('/auth/steam',
+  passport.authenticate('steam'),
+  function(req, res) {
+    // The request will be redirected to Steam for authentication, so
+    // this function will not be called.
+  });
+
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+  
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
